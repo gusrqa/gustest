fb9ce649dbbd36faafb8401d8ad5b15afc50e4f7
gusrqa <73650130+gusrqa@users.noreply.github.com>
2020-10-29 07:33:13 -0600

    Add files via upload

---
+++ EEPROM.ino
@@ -0,0 +1,21 @@
+
+  #include <EEPROM.h>
+
+int Address = 0;
+int Value = 50;
+
+void setup() {
+
+      Serial.begin(9600);
+      EEPROM.write(Address, Value);
+           
+      Serial.print(Address);
+      Serial.print(" ");
+      Serial.println(Value);
+      
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
